49110936d58e9db5e9e2e574e6e63c2611add6bc
geraldik <geraldik1986@gmail.com>
2021-12-26 20:02:31 +0300

    Применена проверка с помощью if-else-if.

--- src/main/java/ru/job4j/oop/Jukebox.java
+++ src/main/java/ru/job4j/oop/Jukebox.java
@@ -4,16 +4,10 @@ public class Jukebox {
 
     public void music(int position) {
         if (position == 1) {
-            System.out.println(
-                    "Пусть бегут неуклюже \n" + "Пешеходы по лужам, \n" + "А вода по асфальту рекой. \n" + "И не ясно прохожим \n"
-                    + "В этот день непогожий \n" + "Почему я весёлый такой.\n");
-        }
-        if (position == 2) {
-            System.out.println(
-                    "Спят усталые игрушки, книжки спят.\n" + "Одеяла и подушки ждут ребят.\n" + "Даже сказка спать ложится,\n"
-                    + "Чтобы ночью нам присниться.\n" + "Ты ей пожелай:\n" + "Баю-бай.\n");
-        }
-        if (position != 1 && position != 2) {
+            System.out.println("Пусть бегут неуклюже.");
+        } else if (position == 2) {
+            System.out.println("Спят усталые игрушки, книжки спят.");
+        } else {
             System.out.println("Песня не найдена");
         }
     }
