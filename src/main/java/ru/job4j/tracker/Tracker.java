ce84897a06cce2518f87d7f12bc67cc3ab84a1f0
geraldik <geraldik1986@gmail.com>
2021-12-31 09:37:32 +0300

    Убраны лишние переменные.

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -60,10 +60,7 @@ public class Tracker {
         int index = indexOf(id);
         if (index != -1) {
             items[index] = null;
-            int start = index + 1;
-            int distPos = index;
-            int length = size - index - 1;
-            System.arraycopy(items, start, items, distPos, length);
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
             rsl = true;
