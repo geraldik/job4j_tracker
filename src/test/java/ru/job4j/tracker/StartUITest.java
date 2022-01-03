42a0ec0143ee3f892282b7a9113041d9583a1e64
geraldik <geraldik1986@gmail.com>
2022-01-03 19:00:05 +0300

    Обновил тесты.

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -32,7 +32,7 @@ public class StartUITest {
                 new String[] {"0", String.valueOf(item.getId()), editedName, "1"}
         );
         UserAction[] actions = {
-                new EditAction(),
+                new EditAction(out),
                 new ExitAction()
         };
         new StartUI(out).init(in, tracker, actions);
@@ -48,7 +48,7 @@ public class StartUITest {
                 new String[] {"0", String.valueOf(item.getId()), "1"}
         );
         UserAction[] actions = {
-                new DeleteAction(),
+                new DeleteAction(out),
                 new ExitAction()
         };
         new StartUI(out).init(in, tracker, actions);
