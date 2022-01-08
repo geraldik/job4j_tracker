cbbcd9f2c6c57b95df921ce677cbfd9e940c4aa6
geraldik <geraldik1986@gmail.com>
2022-01-08 09:26:17 +0300

    Добавлен тест на пустой список в случае не найденого совпадения.

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.search;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import java.util.ArrayList;
 import static org.hamcrest.core.Is.is;
@@ -25,5 +26,15 @@ public class PhoneDictionaryTest {
         );
         ArrayList<Person> persons = phones.find("534");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+
+    @Test
+    public void whenNotFind() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Ivan");
+        assertTrue(persons.isEmpty());
     }
 }
