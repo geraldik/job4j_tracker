bd29b8f1dd4d188fc23099278922c281a052eaca
geraldik <geraldik1986@gmail.com>
2022-01-10 13:54:49 +0300

    Добавлен тест при повторном добавлении.

--- src/test/java/ru/job4j/collection/OrderConvertTest.java
+++ src/test/java/ru/job4j/collection/OrderConvertTest.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
 
 public class OrderConvertTest {
     @Test
@@ -16,5 +17,15 @@ public class OrderConvertTest {
         orders.add(new Order("3sfe", "Dress"));
         HashMap<String, Order> map = OrderConvert.process(orders);
         assertThat(map.get("3sfe"), is(new Order("3sfe", "Dress")));
+    }
+
+    @Test
+    public void whenMultiOrderWithDouble() {
+        List<Order> orders = new ArrayList<>();
+        orders.add(new Order("3sfe", "Dress"));
+        orders.add(new Order("3sfe", "Hut"));
+        orders.add(new Order("3sfe", "Pants"));
+        HashMap<String, Order> map = OrderConvert.process(orders);
+        assertThat(map.size(),  is(1));
     }
 }
