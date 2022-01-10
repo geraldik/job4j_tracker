f9ba507e38b881e7ed87630e9bca00d18c5f6363
geraldik <geraldik1986@gmail.com>
2022-01-10 13:35:32 +0300

    Добавлен тест при повторном добавлении.

--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -4,14 +4,23 @@ import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-
+import  static org.junit.Assert.assertFalse;
 public class PassportOfficeTest {
 
     @Test
-    public void add() {
+    public void addWithoutDouble() {
         Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
         PassportOffice office = new PassportOffice();
         office.add(citizen);
         assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+
+    @Test
+    public void addWithDouble() {
+        Citizen citizen1 = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizen2 = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen1);
+        assertFalse(office.add(citizen2));
     }
 }
