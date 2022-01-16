3fda4f9735cbc82a4025517dbbbfa2c1cca900e3
geraldik <geraldik1986@gmail.com>
2022-01-16 09:23:26 +0300

    Исправления в соответствии с рекомендациями.

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -8,6 +8,6 @@ public class DepDescComp implements Comparator<String> {
         String s1 = o1.split("/")[0];
         String s2 = o2.split("/")[0];
         int rsl = s2.compareTo(s1);
-        return rsl != 0 ? o1.compareTo(o2) : rsl;
+        return rsl == 0 ? o1.compareTo(o2) : rsl;
     }
 }
--- src/main/java/ru/job4j/collection/Departments.java
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -9,10 +9,7 @@ public class Departments {
         for (String value : deps) {
             String start = "";
             for (String el : value.split("/")) {
-                start = start + "/" + el;
-                if (start.startsWith("/")) {
-                    start = start.replaceFirst("/", "");
-                }
+                start += "".equals(start) ? el : "/" + el;
                 tmp.add(start);
             }
         }
