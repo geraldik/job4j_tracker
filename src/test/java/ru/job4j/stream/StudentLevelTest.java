cd6e05d591380feacb35ff61fb2ae4ee40db4dc4
geraldik <geraldik1986@gmail.com>
2022-01-19 21:46:37 +0300

    Правки по стилю.

--- src/test/java/ru/job4j/stream/StudentLevelTest.java
+++ src/test/java/ru/job4j/stream/StudentLevelTest.java
@@ -34,8 +34,8 @@ public class StudentLevelTest {
     public void whenHasNull() {
         List<Student> input = new ArrayList<>();
         input.add(null);
-        input.add(new Student(28,"Pety"));
-        List<Student> expected = List.of(new Student(28,"Pety"));
+        input.add(new Student(28, "Pety"));
+        List<Student> expected = List.of(new Student(28, "Pety"));
         assertThat(StudentLevel.levelOf(input, 10), is(expected));
     }
 }
