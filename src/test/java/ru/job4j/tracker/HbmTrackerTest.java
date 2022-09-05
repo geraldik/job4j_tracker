ce842d13d2e767e9b87ea481e7238090747d3d47
geraldik <geraldik1986@gmail.com>
2022-09-05 23:08:25 +0300

    1. Интеграционное тестирование с Hibernate [#6875]

--- src/test/java/ru/job4j/tracker/HbmTrackerTest.java
+++ src/test/java/ru/job4j/tracker/HbmTrackerTest.java
@@ -1,25 +1,12 @@
 package ru.job4j.tracker;
 
-import org.junit.After;
 import org.junit.jupiter.api.Test;
 
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
 class HbmTrackerTest {
-
-    @After
-    public void wipeTable() throws SQLException {
-        try (HbmTracker hbmTracker = new HbmTracker()) {
-            Item item = hbmTracker.add(new Item("item"));
-            assertThat(hbmTracker.findById(item.getId())).isEqualTo(item);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     @Test
     public void whenSaveItemAndFindByGeneratedIdThenMustBeTheSame() {
@@ -57,14 +44,26 @@ class HbmTrackerTest {
     }
 
     @Test
-    void findAll() {
+    public void whenAddItemAndFindByNameThenMustBeTheSame() {
+        try (HbmTracker hbmTracker = new HbmTracker()) {
+        Item item1 = hbmTracker.add(new Item("item1"));
+        Item item2 = hbmTracker.add(new Item("item2"));
+        Item item3 = hbmTracker.add(new Item("item3"));
+        Item item4 = hbmTracker.add(new Item("item3"));
+        assertThat(hbmTracker.findByName(item3.getName())).isEqualTo(List.of(item3, item4));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Test
-    void findByName() {
-    }
-
-    @Test
-    void findById() {
+    void whenAddItemAndDeleteThenMustBeNull() {
+        try (HbmTracker hbmTracker = new HbmTracker()) {
+            Item item = hbmTracker.add(new Item("item"));
+            hbmTracker.delete(item.getId());
+            assertThat(hbmTracker.findById(item.getId())).isNull();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
